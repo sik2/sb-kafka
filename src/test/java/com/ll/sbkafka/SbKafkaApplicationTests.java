a09d8bede6f3cd48e14fbfee075d5c65360336c6
sik2 <bickwang123@gmail.com>
2025-02-18 23:23:46 +0900

    init

---
+++ src/test/java/com/ll/sbkafka/SbKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sbkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
